2c3113e1dc50314fcd0e43623f30a916334ce71c
Nikolai Efremov <efremovnikolaus@gmail.com>
2023-04-30 21:21:35 +0200

    first commit

---
+++ src/main/java/org/example/User.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class User {
+    public String name;
+    public String surname;
+    public User(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+}
